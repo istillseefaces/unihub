3b6f4decedad1405fc0194bf2ea6c649816325ab
Файрушин Ян <fajrusinan6@gmail.com>
2026-09-11 10:51:53 +0500

    added new category

--- frontend/app.js
+++ frontend/app.js
@@ -4,13 +4,25 @@ const tasks = [
     title: "Calculus 1",
     description: "Complete Assignment 2" ,
     degree: "SCSM", 
-    deadline: "2026-11-09"
+    deadline: "2026-11-09",
+    category: homework
+
     },
     {
         id: 2,
         title: "Physics 2",
         description: "Complete Assignment 3 in Notebook" ,
         degree: "SCSM",
-        deadline: "2026-11-09"
+        deadline: "2026-11-09",
+        catrgory: homework
+    }, 
+    {
+        id: 15,
+        title: "Physics 2",
+        description: "Complete Assignment 3 in Notebook" ,
+        degree: "SCSM",
+        deadline: "2026-11-09",
+        category: exam
     }
 ]
+
