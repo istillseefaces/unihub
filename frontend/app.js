3f43a295d2b7c8d0292c044bf18a450b2063e8f1
Файрушин Ян <fajrusinan6@gmail.com>
2026-09-11 10:47:40 +0500

    added variables with arrays for tasks

---
+++ frontend/app.js
@@ -0,0 +1,16 @@
+const tasks = [
+{
+    id: 1,
+    title: "Calculus 1",
+    description: "Complete Assignment 2" ,
+    degree: "SCSM", 
+    deadline: "2026-11-09"
+    }
+    {
+        id: 2,
+        title: "Physics 2",
+        description: "Complete Assignment 3 in Notebook" ,
+        degree: "SCSM",
+        deadline: "2026-11-09"
+    }
+]
