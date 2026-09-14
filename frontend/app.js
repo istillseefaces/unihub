20162a4c51f6f5c5cad976463b4ecf1c99f24d46
Файрушин Ян <fajrusinan6@gmail.com>
2026-09-14 10:43:59 +0500

    added function which calculate the progress

--- frontend/app.js
+++ frontend/app.js
@@ -58,3 +58,17 @@ function filterTaskByCategory(arr, category) {
 function filterTaskByCompletion(arr, completed) {
         return arr.filter(task => task.completed === completed);
     }
+
+function calculateProgress(aa) {
+    const allTasks = arr.length;
+
+    const completedTasks = filterTaskByCompletion(arr, true).completed
+
+
+    if(allTasks === 0) {
+        return 0;
+    }
+    const progress = (completestasls /allTasks) * 100;
+    return progress;
+}
+
